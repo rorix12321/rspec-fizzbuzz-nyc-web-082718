275a1143c2d205b82886775981fe662dc85a3abe
Ricky Liu <rorix12321@gmail.com>
2018-08-10 03:57:27 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -2,5 +2,10 @@ def fizzbuzz(number)
   if number % 3 == 0 && number % 5 == 0
     return "Fizzbuzz"
   end
-  if number %
+  if number % 3 == 0
+    return "Fizz"
+  end
+if number % 5 == 0
+  return "Buzz"
+  end
 end
